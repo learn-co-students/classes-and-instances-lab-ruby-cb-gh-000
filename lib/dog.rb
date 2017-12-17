0c409d0ea02590df6ba97e437f7143273b2a03f4
Umair Ahmad <ahmadumair17@gmail.com>
2017-12-17 21:36:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog #Abstract data-type representing a template for a dog
+
+end
+
+#Some dogs, instances of the Dog Class
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person #Abstract data-type representing a template for a person
+
+end
+
+#Some famous persons, instances of Person
+adele_goldberg = Person.new
+alan_kay = Person.new
